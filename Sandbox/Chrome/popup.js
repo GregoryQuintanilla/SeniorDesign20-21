5b686a8672b745a2fd68daf2861d352df5903ad5
Gregory Quintanilla <gquintanilla1@pride.hofstra.edu>
2021-01-07 13:45:00 -0600

    A running Sandbox project to create a skeleton of the system we intend to make. it has a working browser extension, firebase hosting, firestore and NodeJS express app.

---
+++ Sandbox/Chrome/popup.js
@@ -0,0 +1,38 @@
+let changeColor = document.getElementById('changeColor');
+let getReq = document.getElementById('getReq');
+// this is used so we have access to the background page of the chrome. 
+// bkg.console.log() will be sent to that page and that is one way to debug things and see fi stuff is formatted correctly.
+var bkg = chrome.extension.getBackgroundPage();
+
+chrome.storage.sync.get('color', function(data) {
+    changeColor.style.backgroundColor = data.color;
+    changeColor.setAttribute('value', data.color);
+})
+// https://google.com works. Defaults as POST for some reason but whatevs.
+// need to figure out why it can't load localhost:5000 or 127.0.0.1:5000. Next step.
+getReq.onclick = function(element) {
+    bkg.console.log("Is this doing anything????");
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        chrome.tabs.executeScript(
+            tabs[0].id,
+            {code: 'var xmlHttp = new XMLHttpRequest();' +
+                   'xmlHttp.open("GET", "http://localhost:5000/senior-design-test-host/us-central/app/",false);' + //'xmlHttp.open("GET", "https://google.com", false);' +
+                   'xmlHttp.send(null);' +
+                   'console.log(xmlHttp.requestText);'
+                });
+            //{code: 'console.log(\''+element.target.value+'\');'});
+        })
+};
+
+
+changeColor.onclick = function(element) {
+    let color = element.target.value;
+    bkg.console.log("Is this doing anything????");
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        chrome.tabs.executeScript(
+            tabs[0].id,
+            {code: 'document.body.style.backgroundColor = "' + color + '";'});
+    });
+};
+
+
---
+++ Sandbox/SeniorDesignTestHost/functions/index.js
@@ -0,0 +1,55 @@
+const functions = require('firebase-functions'); // for hosting
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+const db = admin.firestore();
+const express = require('express'); // for node app
+const app = express(); // for make node app and express app
+
+
+
+
+
+const testDocRef = db.collection('dummies').doc('greg');
+app.get('/',(request, response) =>{
+    // This is a fun important piece. Since we are the owners of the server we can do whatever the hell we want and accept request from whatever hosts. I reccommend
+    // being atleast familiar with what Cross Origin Resource Sharing (CORS) is and the "idea" behind it. It is a pain in the neck for devs because it is enforced by
+    // most browsers. 
+ 
+    response.set('Access-Control-Allow-Origin', '*');
+    response.sendFile('public/index.html',{root: __dirname});
+
+    //now that we can have the browser talk to the server (!!!) this is where just straight dev comes in. Time to play around
+})
+app.get('/addToDB', (request,response) => {
+    testDocRef.set({
+        name: 'greg',
+        age: 22,
+    });
+    response.send('Added data to the db hopefully');
+});
+
+app.get('/timestamp', (request, response) => {
+    response.send(`${Date.now()}`);
+});
+
+app.get('/timestamp-cached',(request, response) => {
+    response.set('Cache-Control','public, max-age=300, s-maxage=600');
+    response.send(`${Date.now()}`);
+});
+
+app.get('/toServer', (request, response) => {
+    //console.log(request)
+    console.log(request.body);
+    console.log(request.params);
+    console.log(request.query);
+    response.send("Your info has been logged");
+    //console.log("----------------------------------TEST--------------------------------")
+    //response.send("This is the way I want to go for things");
+    //response.send("We want to get a url and do stuff with it, let's get the url");
+});
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+
+exports.app = functions.https.onRequest(app);
